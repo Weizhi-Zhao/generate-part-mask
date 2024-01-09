70616ae332266776faebd612cee76018b2ebccb9
zhaoweizhi.zwz@gmail.com <zhaoweizhi.zwz@gmail.com>
2024-01-09 18:29:18 +0800

    fix bug in SAM SLiMe macher

--- src/SAM_SLiMe_macher.py
+++ src/SAM_SLiMe_macher.py
@@ -9,23 +9,32 @@ import pickle
 from binary_mask_IOU import iomax, iou
 from visualizer import show_rle_masks, show_bbox, show_int32_masks
 import matplotlib.pyplot as plt
+from typing import Optional
 
 
 class SAMSLiMeMacher():
-    def __init__(self, coco_ann_dir, dataset_dir, config_dir, visulize=False, SAM_GT_THRES=0.3):
+    def __init__(self, coco_ann_dir, dataset_dir, config_dir, visulize_step: Optional[int]=None, save_vis_result: Optional[str]=None, SAM_GT_THRES=0.3):
         with open(coco_ann_dir, 'r') as f:
             self.coco_anns = json.load(f)
         self.dataset_dir = dataset_dir
-        self.visulize = visulize
         with open(config_dir, 'r') as f:
             self.config = yaml.safe_load(f)
+        self.visulize_step = visulize_step
+        self.save_vis_result = save_vis_result
+        if self.save_vis_result is not None:
+            if not os.path.exists(self.save_vis_result):
+                os.makedirs(self.save_vis_result)
         self.SAM_GT_THRES = SAM_GT_THRES
 
     def process(self):
         annotations = []
+        if self.visulize_step is not None:
+            fig = plt.figure(figsize=(18, 9))
         print(f"SAM SLiMe Macher: processing images of {self.config['category_name']}")
-        for ann in tqdm(self.coco_anns):
-            file_name = ann['image_id'] + '_' + ann['id']
+        for step, ann in enumerate(tqdm(self.coco_anns)):
+            if self.visulize_step is not None and step % self.visulize_step != 0:
+                continue
+            file_name = str(ann['image_id']) + '_' + str(ann['id'])
             if not os.path.exists(os.path.join(self.dataset_dir, file_name + '.png')):
                 print(f"SAM SLiMe Macher error: file {file_name + '.png'} not found")
                 continue
@@ -56,8 +65,8 @@ class SAMSLiMeMacher():
                 'part_annotations': part_anns
             })
 
-            if self.visulize:
-                img = cv2.imread(os.path.join(self.dataset_dir, ann['image_id'] + '_' + ann['id'] + '.png'))
+            if self.visulize_step is not None:
+                img = cv2.imread(os.path.join(self.dataset_dir, str(ann['image_id']) + '_' + str(ann['id']) + '.png'))
                 plt.clf()
                 plt.subplot(1, 2, 1)
                 plt.title('SAM')
@@ -69,12 +78,16 @@ class SAMSLiMeMacher():
                 show_bbox(bboxs, names)
                 plt.subplot(1, 2, 2)
                 plt.title('SLiMe')
-                np_masks = np.load(os.path.join(self.dataset_dir, file_name + '.npy'))
+                np_masks = np.zeros(restricted_SLiMe_masks[0].shape, dtype=np.int32)
+                for r_mask, mask_id in zip(restricted_SLiMe_masks, mask_ids):
+                    np_masks[r_mask] = mask_id
                 show_int32_masks(img, np_masks)
                 plt.draw()
-                plt.waitforbuttonpress()
+                if self.save_vis_result is not None:
+                    plt.savefig(os.path.join(self.save_vis_result, f"{file_name}.png"))
+                plt.waitforbuttonpress(1)
 
-        if not self.visulize:
+        if self.visulize_step is None:
             with open(os.path.join(self.dataset_dir, f"part_annotations_{self.config['category_name']}.json"), 'w') as f:
                 json.dump(annotations, f)
 
@@ -141,16 +154,19 @@ class SAMSLiMeMacher():
     
     def get_cropped_gt_binary_mask(self, ann):
         x1, y1, x2, y2 = ann['position']
-        polygon_mask = ann['segmentation']
-        polygon_mask = np.array(polygon_mask).reshape(-1, 2)
-        polygon_mask = polygon_mask - np.array([x1, y1])
-        polygon_mask = polygon_mask.reshape(1, -1).tolist()
+        polygon_masks = ann['segmentation']
+        final_binary_mask = np.zeros((512, 512), dtype=np.bool_)
+        for polygon_mask in polygon_masks:
+            polygon_mask = np.array(polygon_mask).reshape(-1, 2)
+            polygon_mask = polygon_mask - np.array([x1, y1])
+            polygon_mask = polygon_mask.reshape(1, -1).tolist()
 
-        rle = maskUtils.frPyObjects(polygon_mask, y2-y1+1, x2-x1+1)
-        binary_mask = maskUtils.decode(rle)
-        binary_mask = cv2.resize(binary_mask, (512, 512), interpolation=cv2.INTER_NEAREST)
-        binary_mask = binary_mask.astype(np.bool_)
-        return binary_mask
+            rle = maskUtils.frPyObjects(polygon_mask, y2-y1+1, x2-x1+1)
+            binary_mask = maskUtils.decode(rle)
+            binary_mask = cv2.resize(binary_mask, (512, 512), interpolation=cv2.INTER_NEAREST)
+            binary_mask = binary_mask.astype(np.bool_)
+            final_binary_mask = np.logical_or(final_binary_mask, binary_mask)
+        return final_binary_mask
 
     def restrict_SLiMe_masks(self, SLiMe_masks, gt_mask):
         for i, mask in enumerate(SLiMe_masks):
@@ -160,7 +176,7 @@ class SAMSLiMeMacher():
     def read_SLiMe_masks(self, file_name):
         np_masks = np.load(os.path.join(self.dataset_dir, file_name + '.npy'))
         SLiMe_masks = []
-        mask_ids = np.unique(np_masks)
+        mask_ids = np.unique(np_masks).tolist()
         # ignore background
         mask_ids.remove(0)
         for id in mask_ids:
@@ -169,13 +185,15 @@ class SAMSLiMeMacher():
     
     def part_id_to_name(self, part_id):
         for part_name in self.config['part_id'].keys():
-            if self.config[part_name] == part_id:
+            if self.config['part_id'][part_name] == part_id:
                 return part_name
         print(f"part id to name error: part name {part_name} not found")
         return 'wrong'
     
 '''
-python src/SAM_SLiMe_macher.py --coco_ann_dir datasets/coco/bus_square/annotations_bus.json --dataset_dir datasets/coco/bus_square --config_dir configs/pascalpart_bus.yaml --visulize
+python src/SAM_SLiMe_macher.py --coco_ann_dir datasets/coco/bus_square/annotations_bus.json --dataset_dir datasets/coco/bus_square --config_dir configs/pascalpart_bus.yaml --visulize_step 50 --save_vis_result outputs/coco_bus_result
+
+python src/SAM_SLiMe_macher.py --coco_ann_dir datasets/coco/bus_square/annotations_bus.json --dataset_dir datasets/coco/bus_square --config_dir configs/pascalpart_bus.yaml
 '''
 if __name__ == '__main__':
     import argparse
@@ -183,7 +201,9 @@ if __name__ == '__main__':
     parser.add_argument('--coco_ann_dir', type=str, required=True)
     parser.add_argument('--dataset_dir', type=str, required=True)
     parser.add_argument('--config_dir', type=str, required=True)
-    parser.add_argument('--visulize', action='store_true')
+    # parser.add_argument('--visulize_step', type=int, default=1)
+    # parser.add_argument('--save_vis_result', type=str, required=True)
     args = parser.parse_args()
-    matcher = SAMSLiMeMacher(args.coco_ann_dir, args.dataset_dir, args.config_dir, args.visulize)
+    # matcher = SAMSLiMeMacher(args.coco_ann_dir, args.dataset_dir, args.config_dir, args.visulize_step, args.save_vis_result)
+    matcher = SAMSLiMeMacher(args.coco_ann_dir, args.dataset_dir, args.config_dir)
     matcher.process()
